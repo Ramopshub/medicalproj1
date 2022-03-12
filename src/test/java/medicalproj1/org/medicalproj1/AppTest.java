747cac1667ce8c5d9ea185dc3edf27852e833fd5
Ram <Dev1_git@gmail.com>
2022-03-12 10:42:26 +0530

    commit from LR

---
+++ src/test/java/medicalproj1/org/medicalproj1/AppTest.java
@@ -0,0 +1,20 @@
+package medicalproj1.org.medicalproj1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
